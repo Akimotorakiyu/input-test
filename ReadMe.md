ccf63febaa4bd68eeb5a1895b76e7f66faf1de6c
湫曗 <autumnsilverleaves@gmail.com>
2021-06-06 14:16:58 +0800

    add input 总结

--- ReadMe.md
+++ ReadMe.md
@@ -1,18 +1,20 @@
 # Input-test
 
-## InputEvent.isComposing
+## without keyboardevent
+
+### InputEvent.isComposing
 
 | chrome | firefox | safari |
 | ------ | ------- | ------ |
 | ✅     | ✅      | ✅     |
 
-## input alphabeta
+### input alphabeta
 
 | chrome     | firefox    | safari     |
 | ---------- | ---------- | ---------- |
 | insertText | insertText | insertText |
 
-## input chinese 你
+### input chinese 你
 
 | chrome                               | firefox                              | safari                                     |
 | ------------------------------------ | ------------------------------------ | ------------------------------------------ |
@@ -24,3 +26,14 @@
 | compositionupdate [你]               | compositionupdate [你]               | input deleteCompositionText undefined null |
 | input insertCompositionText false 你 | compositionend [你]                  | input insertFromComposition undefined [你] |
 | compositionend [你]                  | input insertCompositionText false 你 | compositionend [你]                        |
+
+#### 综上
+
+处理 input 输入
+
+1. alphabeta 监听 input 事件 inputType 取 insertText
+2. 处理中文 监听 inpute 事件
+   1. chrome firefox inputType 取 insertCompositionText isComposing 取 false
+   2. safari inputType 取 insertFromComposition isComposing
+
+## with keyboard event
--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,40 @@ import "virtual:windi.css";
 
 const element = document.getElementById("container");
 
+const logEvent = false;
+
 element.addEventListener("compositionstart", (event) => {
-  console.log("compositionstart", event.data, event);
+  console.log("compositionstart", event.data, logEvent ? event : " ");
 });
 element.addEventListener("compositionupdate", (event) => {
-  console.log("compositionupdate", event.data, event);
+  console.log("compositionupdate", event.data, logEvent ? event : " ");
 });
 element.addEventListener("compositionend", (event) => {
-  console.log("compositionend", event.data, event);
+  console.log("compositionend", event.data, logEvent ? event : " ");
 });
 element.addEventListener("input", (_event) => {
   const event = _event as InputEvent;
-  console.log("input", event.inputType, event.isComposing, event.data, event);
+  console.log(
+    "input",
+    event.inputType,
+    event.isComposing,
+    event.data,
+    logEvent ? event : " "
+  );
 });
 element.addEventListener("change", (_event) => {
   const event = _event as InputEvent;
-  console.log("change", event.inputType, event.isComposing, event.data, event);
+  console.log(
+    "change",
+    event.inputType,
+    event.isComposing,
+    event.data,
+    logEvent ? event : " "
+  );
+});
+element.addEventListener("keydown", (event) => {
+  console.log("keydown", event.key, event.isComposing, logEvent ? event : " ");
+});
+element.addEventListener("keyup", (event) => {
+  console.log("keydown", event.key, event.isComposing, logEvent ? event : " ");
 });
